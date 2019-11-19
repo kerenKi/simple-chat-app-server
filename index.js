84327fdc2efc215852ab5c2a3c62de840119de2c
kerenKi <kkinberg13@gmail.com>
2019-11-19 20:54:26 +0100

    Show who joined the chat

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app
 .use(bodyParser.json())
 .use(router)
 
+let usersJoined = []
 
 function onListen () {
   console.log(`Listening on port 4000`)
@@ -22,10 +23,18 @@ io = socketIo.listen(server)
 
 io.on('connection', function(socket){
   console.log('a user connected')
+
+  socket.on('newUser',function(user_name){
+    console.log(user_name)
+    usersJoined.push(user_name)
+    socket.emit('addUserToChat', usersJoined)
+  })
   socket.on('disconnect', function(){
     console.log('user disconnected')
   })
 })
+
+
 
 router.post('/messages', (req, res) => {
   const user_message = req.body.message
